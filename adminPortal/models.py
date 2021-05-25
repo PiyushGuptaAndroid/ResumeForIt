766f877b6867fe6bee1ccdb7464d98ea03cc666a
nikita.rathore <nikita.rahthore@hashedin.com>
2021-05-25 10:25:52 +0530

    local changes

--- adminPortal/models.py
+++ adminPortal/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Question(models.Model):
+    question = models.TextField()
+    optionA = models.TextField()
+    optionB = models.TextField()
+    optionC = models.TextField()
+    optionD = models.TextField()
+    correctOption = models.TextField()
+    tag = models.TextField()
--- studentPortal/views.py
+++ studentPortal/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from adminPortal.models import Question
+from json import dumps
+from django.core import serializers
 
 
 # Create your views here.
 def startquiz(request):
     return render(request, 'studentPortal/quiz/start.html')
 
+
 def quiz(request):
-    return render(request, 'studentPortal/quiz/quiz.html')
+    ques = serializers.serialize(
+        "json", Question.objects.filter(tag__in=['C++', 'Python']).order_by('?')[:4])
+    print()
+    questionsJson = dumps(ques)
+    return render(request, 'studentPortal/quiz/quiz.html', {'questionsData': questionsJson})
+
 
 def endquiz(request):
     return render(request, 'studentPortal/quiz/end.html')
 
+
 def studentDashboard(request):
     return render(request, 'studentPortal/studentDashboard.html')
-
